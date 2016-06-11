5da85bb47abea5da890508b124a1aff79dc3fc70
Adrian Reuter <reutadri@icloud.com>
2016-06-11 14:04:38 +0200

    removed request module which got useless

--- shodan.js
+++ shodan.js
@@ -1,13 +1,8 @@
 var shodan = {};
-var request = require("request");
+
 
 shodan.search = function(req, callback){
-  request
-      .get("https://api.shodan.io/shodan/host/search?key=" + this.settings.key + "&query=" + req.param("query"))
-      .on("response", function(res){
-        // after we got a response from the shodan api we callback it
-        callback(res);
-      });
+  
 }
 
 module.exports = shodan;
