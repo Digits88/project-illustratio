f10a1fcd0147461ea2b047f81035354c76222c4d
Adrian Reuter <reutadri@icloud.com>
2016-06-11 22:25:47 +0200

    some little improvements for the interface

--- app/js/chart.js
+++ app/js/chart.js
@@ -1,89 +1,28 @@
-InitChart();
-
-function InitChart() {
-
-  var barData = [{
-    'x': "Berlin",
-    'y': 0.0001*609685
-  }, {
-    'x': "Hamburg",
-    'y': 0.0001*296689
-  }, {
-    'x': "München",
-    'y': 0.0001*281409
-  }, {
-    'x': "Köln",
-    'y': 0.0001*984
-  }, {
-    'x': "Frankfurt am Main",
-    'y': 0.0001*494434
-  }, {
-    'x': "Stuttgart",
-    'y': 0.0001*108117
-  }, {
-    'x': "Dortmund",
-    'y': 0.0001*96401
-  }, {
-    'x': "Essen",
-    'y': 0.0001*84615
-  }, {
-    'x': "Bremen",
-    'y': 0.0001*75486
-  }, {
-    'x': "Leipzig",
-    'y': 0.0001*92152
-  }];
-
-  var vis = d3.select('#visualisation'),
-    WIDTH = 1000,
-    HEIGHT = 500,
-    MARGINS = {
-      top: 20,
-      right: 20,
-      bottom: 20,
-      left: 100
-    },
-    xRange = d3.scale.ordinal().rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1).domain(barData.map(function (d) {
-      return d.x;
-    })),
-
-
-    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,
-      d3.max(barData, function (d) {
-        return d.y;
-      })
-    ]),
-
-    xAxis = d3.svg.axis()
-      .scale(xRange)
-      .tickSize(5)
-      .tickSubdivide(true),
-
-    yAxis = d3.svg.axis()
-      .scale(yRange)
-      .tickSize(5)
-      .orient("left")
-      .tickSubdivide(true);
-
-
-  vis.append('svg:g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
-    .call(xAxis);
-
-  vis.selectAll('rect')
-    .data(barData)
-    .enter()
-    .append('rect')
-    .attr('x', function (d) {
-      return xRange(d.x);
-    })
-    .attr('y', function (d) {
-      return yRange(d.y);
-    })
-    .attr('width', xRange.rangeBand())
-    .attr('height', function (d) {
-      return ((HEIGHT - MARGINS.bottom) - yRange(d.y));
-    })
-    .attr('fill', 'grey')
-}
+  window.onload = function () {
+  	var chart = new CanvasJS.Chart("chartContainer", {
+  		theme: "theme1",//theme1
+  		title:{
+  			text: "Die größten Städte Deutschlands von links nach rechts mit Anzahl von potenziell Angreifbaren Servern"
+  		},
+  		animationEnabled: false,   // change to true
+  		data: [
+  		{
+  			// Change type to "bar", "area", "spline", "pie",etc.
+  			type: "column",
+  			dataPoints: [
+  				{ label: "Berlin",  y: 609685  },
+  				{ label: "Hamburg", y: 296689  },
+  				{ label: "München", y: 281409  },
+  				{ label: "Frankfurt am Main",  y: 494434  },
+          { label: "Stuttgart",  y:  108117 },
+          { label: "Dortmund",  y: 96401  },
+          { label: "Essen",  y: 84615  },
+          { label: "Bremen",  y: 75486  },
+          { label: "Leipzig",  y: 92152  },
+          { label: "Dresden", y: 99976}
+  			]
+  		}
+  		]
+  	});
+  	chart.render();
+  }
