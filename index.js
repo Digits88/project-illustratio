6ef57515c8a3a5e67d1f1b2a214ddbd64948790c
Adrian Reuter <reutadri@icloud.com>
2016-06-12 09:56:24 +0200

    commented our code

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+/*
+ * This site is the main backend script written in Node.js.
+ * Its use is to route the static files and handle our API.
+ * written by Adrian Reuter github.com/reuta <reutadri@gmx.de>
+ */
+
 var express = require("express");
 var shodan = require("./shodan");
-shodan.settings = {
-  key: "I0eJHqlXu2cAwx99XA8LgKcgf6VvhOrt"
-}
 var app = express();
 
+// first allow to access the static files inside our app directory
 app.use("/app", express.static("app"));
 
+// index route
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/app/index.html");
+  res.sendFile(__dirname + "/app/index.html");    // send back the index.html file
 });
 
-app.get("/api/:city", function(req, res){
+app.get("/api/:city", function(req, res){   // when the browser makes an AJAX call to our city API
+  // terminal message for Debugging
+  //console.log("**SERVER NOTE** Searching for " + req.params.city);
   // request to the shodan site
-  console.log("**SERVER NOTE** Searching for " + req.params.city);
   shodan.search(req, function(json){
-    console.log(json);
+    // send back to browser the json data
     res.json(json);
   });
 });
--- shodan.js
+++ shodan.js
@@ -1,9 +1,17 @@
+/*
+ *  This script was made to communicate between the shodan python script and
+ *  the node.js backend code. This was done using the node.js child_process module.
+ *  written by Adrian Reuter github.com/reuta <reutadri@gmx.de>
+ */
+
 var shodan = {};
 var exec = require('child_process').exec;
 
 shodan.search = function(req, callback){
+  // call the python script and callback its output
+  // NOTE: Weird "'" ticks are added to prevent bugs with cities which contain spaces
   exec("python3.5 python_modules/shodanapi.py " + "'" + req.params.city + "'", function(err, stdout, stderr){
-    callback(JSON.parse(stdout));
+    callback(JSON.parse(stdout)); // callback the stdout and convert it to json before that
   });
 }
 
